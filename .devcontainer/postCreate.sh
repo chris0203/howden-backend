7d0005a2b27dafb7d506367dbe563ae15a82605e
Chris Wong <chris.wong@nittan.com.hk>
2025-10-27 15:41:19 +0800

    init commit

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cp .env.local .env
+composer install -n --no-scripts
+
+# Clearing cache
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+php artisan event:cache
+
+php artisan migrate --force
+php artisan db:seed --force
+
+php artisan vendor:publish --force --tag=livewire:assets
---
+++ README.md
@@ -0,0 +1,7 @@
+# Howden Private Wealth - Backend
+
+This project was generated with [Laravel](https://laravel.com/docs/12.x/installation)
+
+## Development server
+
+
